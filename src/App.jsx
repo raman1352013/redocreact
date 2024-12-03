6ea44ed2f74e46879039def5a3af25ae97e6ff54
Raman <ramanthakur13135@gmail.com>
2024-12-03 14:52:24 +0530

    ye hoga harish

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import './App.css';
 import Login from './Login';
 import ApiDocs from './ApiDocs';
 
-import { Routes, Route ,Link} from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 
 function App() {
   return (
    
       <> 
-        <nav>
-        <Link to="/">543534543Login</Link> | <Link to="/apiDocs">API Docs</Link>
-      </nav>
+       
+      <Login></Login>
         <p>This is app</p>
         <Routes>
-          <Route path="/" element={<Login />} />
+          
           <Route path="/apiDocs" element={<ApiDocs />} />
         </Routes>
       </>
