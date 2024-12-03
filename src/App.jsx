df27a6f08ae0ab42ffeea358d6e38ee4fffacc49
harish <harish.rajjput@gmail.com>
2024-12-03 13:58:54 +0530

    select css changed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
        <nav>
         <Link to="/">Login</Link> | <Link to="/apiDocs">API Docs</Link>
       </nav>
-        <p>This is app</p>
+      
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/apiDocs" element={<ApiDocs />} />
