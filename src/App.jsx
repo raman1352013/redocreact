0e4eee51c70e3606ba61957bcf981aa7012b8077
Raman <ramanthakur13135@gmail.com>
2024-12-05 10:34:37 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import './App.css';
-import Login from './Login';
-import ApiDocs from './ApiDocs';
-
-import { Routes, Route ,Link} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import ApiDocs from "./ApiDocs";
 
 function App() {
   return (
-   
-      <> 
-        <nav>
-        <Link to="/">Login</Link> 
-      </nav>
-      
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/apiDocs" element={<ApiDocs />} />
-        </Routes>
-      </>
-
+    <Router>
+      <Routes>
+        {/* Login screen is the default route */}
+        <Route path="/" element={<Login />} />
+        {/* Protected route to the ApiDocs */}
+        <Route path="/apiDocs" element={<ApiDocs />} />
+      </Routes>
+    </Router>
   );
 }
 
